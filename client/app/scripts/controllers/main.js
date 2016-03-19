4245d02ee131c1857ae32b712b661290787354d8
vuongc <christopher.k.vuong@gmail.com>
2016-03-20 00:40:02 +0800

    Add a error block

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -2,9 +2,10 @@ angular.module('app').controller('MainController', ['$scope', '$http', '$log', f
 
   $scope.title = 'Choose your answer';
   $scope.database = {};
-  $scope.choices = [];
+  $scope.notification = {};
+  $scope.error = false;
 
-
+  $log.info($scope.notification);
 
   // Get the answers object from server
   var getChoices = function () {
@@ -13,9 +14,33 @@ angular.module('app').controller('MainController', ['$scope', '$http', '$log', f
       url: 'http://localhost:8080/choices'
     }).then(function successCallback (response) {
       $scope.database = response.data;
-      $scope.choices = response.data.choices;
+      $scope.notification = {type: 'success', message: 'You got the object !'};
+      $scope.error = false;
     }, function errorCallback (response) {
       $log.info('Error: ', response);
+      $scope.notification = {type: 'error', message: 'You didn\'t get the object !'};
+      $scope.error = true;
+    });
+  };
+
+  $scope.sendAnswer = function (answer) {
+    $http({
+      method: 'POST',
+      url: 'http://localhost:8080/answer',
+      data: { version: $scope.database.version, choice: answer }
+    }).then(function successCallback (response) {
+      if (!response.answer) {
+        $scope.notification = {type: 'error', message: 'Too bad it wasn\'t the good answer !'};
+        $scope.error = true;
+      } else {
+        $scope.notification = {type: 'success', message: 'Too bad it wasn\'t the good answer !'};
+        $scope.error = false;
+      }
+      $scope.database = response.data;
+    }, function errorCallback (response) {
+      $log.info('Error: ', response);
+      $scope.notification = {type: 'error', message: 'You didn\'t send your answer !'};
+      $scope.error = true;
     });
   };
 
