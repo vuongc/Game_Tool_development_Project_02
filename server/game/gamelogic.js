f2581fb561ed84bb96f9e348005c2ab03325dd95
michau_k <alex.michaux@epitech.eu>
2016-03-19 12:57:50 -0400

    Update client when its version is behind the server

--- server/game/gamelogic.js
+++ server/game/gamelogic.js
@@ -3,6 +3,10 @@ const updateSecret = 'steaktartare';
 var database = require('../database/answers.json');
 var serverVersion = database.server_version;
 
+var getInfoServer = function () {
+  return ({ version: database.server_version, choices: database.choices });
+};
+
 var updateDb = function (secret, done) {
   if (secret === updateSecret) {
     delete require.cache[require.resolve('../database/answers.json')];
@@ -16,11 +20,17 @@ var updateDb = function (secret, done) {
 };
 
 var getResult = function (choice, version, done) {
-  if (database.answer[String(version)] == choice)
-    done(true);
-  else
-    done(false);
+  var answer = (database.answer[String(version)] == choice);
+  if (version == serverVersion) {
+    done({ answer: answer });
+  } else {
+    done({ answer: answer, update: {
+        version: serverVersion,
+        choices: database.choices,
+      }, });
+  }
 };
 
+module.exports.getInfoServer = getInfoServer;
 module.exports.getResult = getResult;
 module.exports.updateDb = updateDb;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,12 +3,12 @@ const router = require('express').Router();
 const game = require('../game/gamelogic');
 
 router.get('/choices', function (req, res) {
-  res.json(database);
+  res.json(game.getInfoServer());
 });
 
 router.post('/play', function (req, res) {
   game.getResult(req.body.choice, req.body.version, function (result) {
-    res.json({ answer: result });
+    res.json(result);
   });
 });
 
