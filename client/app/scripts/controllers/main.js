7610e3245f7bc24e1bd1d11534b3f9ce9e2c8ecf
vuongc <christopher.k.vuong@gmail.com>
2016-03-19 22:12:14 +0800

    Remove logs when successfully retrieve data from server and save title on

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,15 +1,18 @@
 angular.module('app').controller('MainController', ['$scope', '$http', '$log', function ($scope, $http, $log) {
-  var _this = this;
 
-  _this.title = 'Choose your answer';
+  $scope.title = 'Choose your answer';
+  $scope.database = {};
   $scope.choices = [];
 
+
+
+  // Get the answers object from server
   var getChoices = function () {
     $http({
       method: 'GET',
       url: 'http://localhost:8080/choices'
     }).then(function successCallback (response) {
-      $log.info('Success: ', response);
+      $scope.database = response.data;
       $scope.choices = response.data.choices;
     }, function errorCallback (response) {
       $log.info('Error: ', response);
